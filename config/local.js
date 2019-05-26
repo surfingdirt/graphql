9ab2c06d2f862ca6973f1733b72ee0da7aadecd3
Mikael Gramont <mgramont@gmail.com>
2019-05-26 16:12:57 +0200

    Point to the local dev environment, not test.

--- config/local.js
+++ config/local.js
@@ -1,6 +1,6 @@
 module.exports = {
-  apiUrl: 'http://test.api.ridedb.wrk',
-  storageLocalDomain: 'http://www.local.wrk/RideDB/public',
+  apiUrl: 'http://api.ridedb.wrk',
+  storageLocalDomain: 'http://api.ridedb.wrk',
   storageLocalPath: 'files',
   port: 4000,
   tmpFolder: '/tmp',
