d3ddfe2bff69419ebfe6e79907debc91e347c661
Mikael Gramont <mgramont@gmail.com>
2020-02-10 18:19:24 +0100

    More reliable language management

--- src/utils/language.js
+++ src/utils/language.js
@@ -2,6 +2,14 @@ const DEFAULT_LOCALE = 'en-US';
 
 module.exports = {
   findContentVersionForLocale: (sourceParts, localeOptions) => {
+    if (sourceParts === null) {
+      return '';
+    }
+
+    if (typeof sourceParts === 'string') {
+      return sourceParts;
+    }
+
     // User preference first
     for (let i = 0; i < localeOptions.length; i++) {
       const currentLocale = `${localeOptions[i].code}-${localeOptions[i].region}`;
