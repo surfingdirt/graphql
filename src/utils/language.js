51b9d8ea9b36ef8ded21164f57d88c003bc098ac
Mikael Gramont <mgramont@gmail.com>
2020-02-10 18:05:46 +0100

    Added fallback to same language code (ie, en-GB will fallback to en-US)

--- src/utils/language.js
+++ src/utils/language.js
@@ -10,6 +10,17 @@ module.exports = {
         }
     }
 
+    // Same code?
+    for (let i = 0; i < localeOptions.length; i++) {
+      const currentCode = `${localeOptions[i].code}-`;
+      const found = Object.entries(sourceParts).find(([locale]) => {
+        return locale.indexOf(currentCode) === 0;
+      });
+      if (found) {
+        return found[1];
+      }
+    }
+
     // Default locale
     if (typeof sourceParts[DEFAULT_LOCALE] !== 'undefined') {
       return sourceParts[DEFAULT_LOCALE];
