3a32948229c5e37d6c7f89b6bd027a22d55a9b62
Mikael Gramont <mgramont@gmail.com>
2020-08-30 19:32:33 +0200

    Updated docker image id to 52

--- config/index.js
+++ config/index.js
@@ -2,8 +2,8 @@ const env = process.env.NODE_ENV || 'local';
 
 const conf = {
   local: {
-    apiUrl: 'http://localhost:8007',
-    //apiUrl: 'http://localhost:8008', // test
+    //apiUrl: 'https://api.surfingdirt.com', // prod
+    apiUrl: 'http://localhost:8007', // local
     // storageLocalDomain: 'http://localhost:8007', // local
     storageLocalDomain: 'https://apisurfingdirt.b-cdn.net',
     storageLocalPath: 'files',
