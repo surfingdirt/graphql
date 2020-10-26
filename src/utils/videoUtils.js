8d3d3a95d6f3d2aad7954cc2f562b9bd475fa859
Mikael Gramont <mgramont@gmail.com>
2020-10-26 10:23:22 +0100

    Typo

--- src/utils/videoUtils.js
+++ src/utils/videoUtils.js
@@ -205,9 +205,9 @@ const getVideoInfo = async (input) => {
 
   if (mediaSubType === INSTAGRAM) {
     console.log('getVideoInfo INSTAGRAM');
-    console.log(redirectUrls.length > 0 ? 'Looks like IP ban is in place', 'No IP ban');
+    console.log(redirectUrls.length > 0 ? 'Looks like IP ban is in place' : 'No IP ban');
     console.log('statusCode');
-    console.log(statusCode);
+    console.log(statusCode);vim
     console.log('request headers');
     console.log(JSON.stringify(originalRequest.gotOptions.headers, null, 2));
     console.log('redirectUrls');
